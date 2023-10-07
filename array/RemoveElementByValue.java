2d980a3386a09e5c74cb9d9542619ef16cf845cd
Hoang Vu Hung <hoanghung250604@gmail.com>
2023-10-07 21:06:50 +0700

    modifier spell

--- array/RemoveElementByValue.java
+++ array/RemoveElementByValue.java
@@ -1,10 +1,5 @@
 package array;
 
-<<<<<<< HEAD
-=======
-import java.lang.reflect.Array;
-import java.lang.reflect.*;
->>>>>>> 79c1be6547a75334ff2720bb3f61b07a9096b4b1
 // Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
 // Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
 // Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
