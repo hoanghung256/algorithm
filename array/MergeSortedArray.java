f889fd802378b456cf00053c5543c64e0dbe8334
Hoang Vu Hung <hoanghung250604@gmail.com>
2023-10-11 01:00:41 +0700

    in process

--- array/MergeSortedArray.java
+++ array/MergeSortedArray.java
@@ -1,5 +1,8 @@
 package array;
 
+import java.util.Collection;
+import java.util.Collections;
+
 // You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
 // Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 // The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
@@ -35,10 +38,32 @@ package array;
 
 public class MergeSortedArray {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0;
+        int j = 0;
+
+        while (i < m + n) {
+            if (n == 0) break;
+            if (m == 0) {
+                nums1[0] = nums2[0];
+                break;
+            }
+            if (nums1[i] == 0) {
+                nums1[i] = nums2[j];
+                j++;
+            }
+            i++;
+        } 
         
     }  
 
     public static void main(String[] args) {
-        
+       int[] nums1 = {1,2,3,0,0,0};
+       int[] nums2 = {2,5,6};
+
+       merge(nums1, 3, nums2, 3);
+
+       for (int n : nums1) {
+        System.out.print(n + " ");
+       }
     }
 }
