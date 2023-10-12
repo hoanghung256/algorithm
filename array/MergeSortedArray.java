fefa767c2a03e99f0cd780c2d9f0261748991570
Hoang Vu Hung <hoanghung250604@gmail.com>
2023-10-12 23:01:48 +0700

    Done Merge Sorted Array but need review more

--- array/MergeSortedArray.java
+++ array/MergeSortedArray.java
@@ -1,8 +1,5 @@
 package array;
 
-import java.util.Collection;
-import java.util.Collections;
-
 // You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
 // Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 // The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
@@ -25,7 +22,6 @@ import java.util.Collections;
 // Explanation: The arrays we are merging are [] and [1].
 // The result of the merge is [1].
 // Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
- 
 
 // Constraints:
 // nums1.length == m + n
@@ -38,32 +34,33 @@ import java.util.Collections;
 
 public class MergeSortedArray {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = 0;
-        int j = 0;
-
-        while (i < m + n) {
-            if (n == 0) break;
-            if (m == 0) {
-                nums1[0] = nums2[0];
-                break;
+        m--;
+        n--;
+        int i = nums1.length - 1;
+        while (i >= 0) {
+            if (m < 0) {        //when element of first array is less than 0, we need to add element from second array
+                nums1[i] = nums2[n--];
+            } else if (n < 0) {         //when element of second array is less than 0, we need to add element from first array
+                nums1[i] = nums1[m--];
+            } else {
+                if (nums1[m] < nums2[n]) {      
+                    nums1[i] = nums2[n--];
+                } else {
+                    nums1[i] = nums1[m--];
+                }
             }
-            if (nums1[i] == 0) {
-                nums1[i] = nums2[j];
-                j++;
-            }
-            i++;
-        } 
-        
-    }  
+            i--;
+        }
+    }
 
     public static void main(String[] args) {
-       int[] nums1 = {1,2,3,0,0,0};
-       int[] nums2 = {2,5,6};
+        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+        int[] nums2 = { 2, 5, 6 };
 
-       merge(nums1, 3, nums2, 3);
+        merge(nums1, 3, nums2, 3);
 
-       for (int n : nums1) {
-        System.out.print(n + " ");
-       }
+        for (int n : nums1) {
+            System.out.print(n + " ");
+        }
     }
 }
