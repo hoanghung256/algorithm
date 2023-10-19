338d3b60eba0ec8892db2fb8338871211adcd618
Hoang Vu Hung <hoanghung250604@gmail.com>
2023-10-19 23:16:14 +0700

    Solved twopointer.TwoSum2 with 3 approach

--- twopointer/TwoSum2.java
+++ twopointer/TwoSum2.java
@@ -1,5 +1,9 @@
 package twopointer;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 // Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 < numbers.length.
 // Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
 // The tests are generated such that there is exactly one solution. You may not use the same element twice.
@@ -28,8 +32,81 @@ package twopointer;
 // The tests are generated such that there is exactly one solution.
 
 public class TwoSum2 {
-    public int[] twoSum(int[] numbers, int target) {
-        
+    //Two pointers approach -> TC: O(n) SC: O(1)
+    public static int[] twoSumII(int[] nums, int target) {      
+        int left = 0;
+        int right = nums.length - 1;
+        int sum = 0;
+
+        while (left < right) {
+            sum = nums[left] + nums[right];
+            if (sum == target) {
+                return new int[] {left+1, right+1};
+            } else if (sum < target) {
+                left++;
+                continue;
+            }
+            right--;
+        }
         return new int[] {};
+    }
+
+    //Binary search approach -> TC: O(n.log(n)) SC: O(1)
+    public static int[] twoSumII2(int[] nums, int target) {     
+        int left = 0;
+        int right = nums.length-1;
+        int mid = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            left = i + 1;
+            right = nums.length-1;
+            while (left <= right) {
+                mid = left + (right-left)/2;
+                if (nums[mid] == target - nums[i]) {
+                    return new int[] {i+1, mid+1};
+                } else if (nums[mid] < target - nums[i]) {
+                    left = mid + 1;
+                    continue;
+                }
+                right = mid - 1;
+            }
+        }
+        return new int[] {};
+    }
+
+    //HashMap approach -> TC: O(n) SC: O(n). However Twopointers is better than HashMap because of using additional data structure: HashMap
+    public static int[] twoSumII3(int[] nums, int target) {  
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (hm.containsKey(diff)) {
+                return new int[] {i, hm.get(diff)+1};
+            } 
+            hm.putIfAbsent(nums[i], i+1);
+        }
+        return new int[] {};
+    }
+
+
+    public static void main(String[] args) {
+        int[] a = {2,7,11,15};
+        int[] b = {1,2,3,4,5,6};
+        int[] c = {-1,0};
+
+
+        int[] r1 = twoSumII3(a, 9);
+        System.out.print("Test case 1 (expected {1,2}): ");
+        Arrays.stream(r1).forEach(e -> System.out.print(e + " "));
+        System.out.println();
+
+        int[] r2 = twoSumII3(b, 11);
+        System.out.print("Test case 2 (expected {5,6}): ");
+        Arrays.stream(r2).forEach(e -> System.out.print(e + " "));
+        System.out.println();
+
+        int[] r3 = twoSumII3(c, -1);
+        System.out.print("Test case 3 (expected {1,2}): ");
+        Arrays.stream(r3).forEach(e -> System.out.print(e + " "));
     }
 }
