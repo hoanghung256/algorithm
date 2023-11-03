ff91d9f58b17659308540199d987cc898b3d48c1
Hoang Vu Hung <hoanghung250604@gmail.com>
2023-11-03 15:28:42 +0700

    Solved problem 2/3 of LG Fresher SE test

--- array/FindPrimes.java
+++ array/FindPrimes.java
@@ -5,10 +5,10 @@ import java.util.HashSet;
 public class FindPrimes {
     public static int solution(int[] nums) {
         HashSet<Integer> hs = new HashSet<>();
-        for (int i = 0; i < nums.length-1; i++) {
-            for (int j = i+1; j < nums.length; j++) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 if (isPrime(nums[i] + nums[j])) {
-                   hs.add(nums[i] + nums[j]);       
+                    hs.add(nums[i] + nums[j]);
                 }
             }
         }
@@ -17,7 +17,7 @@ public class FindPrimes {
 
     public static boolean isPrime(int n) {
         for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {       //return false if i is divisor of n
+            if (n % i == 0) { // return false if i is divisor of n
                 return false;
             }
         }
@@ -25,7 +25,9 @@ public class FindPrimes {
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4};
-        System.out.println("Test case 1: " + solution(nums));
+        int[] a = {1, 2, 3, 4};
+        int[] b = {2, 3, 4, 5};
+        System.out.println("Test case 1: " + solution(a));
+        System.out.println("Test case 2: " + solution(b));
     }
 }
