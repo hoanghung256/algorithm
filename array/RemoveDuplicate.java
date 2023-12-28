a25bae62e74927b3fa9cb5a45879a26b7dd4eab3
Hoang Vu Hung <hoanghung250604@gmail.com>
2023-12-29 00:14:14 +0700

    Delete unnecessary files

--- array/RemoveDuplicate.java
+++ array/RemoveDuplicate.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.stream.Collectors;
 
-//You are given s string consisting off lowercase English letters. 
+//You are given s string consisting of lowercase English letters. 
 //A duplicate removal consists of choosing two adjacent and equal letters and removing them.
 
 //Example 1: 
@@ -12,7 +12,8 @@ import java.util.stream.Collectors;
 //Output: "ca"
 
 public class RemoveDuplicate {
-    public static String removeDoubleDuplicate(String s) {
+    //HashSet approach
+    public static String removeDuplicate1(String s) {
         HashSet<Character> hs = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
             if (hs.contains(s.charAt(i))) {
@@ -26,26 +27,45 @@ public class RemoveDuplicate {
         return result;
     }
 
+    //Two pointer approach
+    public static String removeDuplicate2(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        int j = 1;
+
+        while (j < sb.length()) {
+            char a = sb.charAt(0);
+            char b = sb.charAt(j);
+            if (a != b) {
+                j++;
+            } else if (a == b) {
+                sb.deleteCharAt(0);
+                sb.deleteCharAt(j - 1);     //Because delete char at 0, the index of duplicate char minus to 1 
+                j = 1;
+            }
+        }
+        return sb.toString();
+    }
+
+    //HashMap approach
     public static String removeKDuplicate(String s, int k) {
         HashMap<Character, Integer> hm = new HashMap<>();
         String result = new String(s);
+
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            int count = hm.getOrDefault(c, 0) + 1; // First occurrence will be set default by 0
-            if (count == k) {
-                result = result.replaceAll(Character.toString(c), "");
+            int frequency = hm.getOrDefault(c, 0) + 1; // First occurrence will be set default by 0
+            if (frequency == k) {
+                result = result.replaceAll(c + "", "");
                 continue;
             }
-            hm.put(c, count);
+            hm.put(c, frequency);
         }
 
         return result;
     }
 
     public static void main(String[] args) {
-        // System.out.println("Testcase 1 (expected ca): " +
-        // removeDoubleDuplicate("abbaca"));
-        System.out.println("Testcase 2 (expected aa): " +
-        removeKDuplicate("deeedbdbcccbaa", 3));
+        // System.out.println("Testcase 1 (expected ca): " + removeDuplicate2("abbaca"));
+        System.out.println("Testcase 2 (expected caa): " + removeKDuplicate("deeedbdbcccbcaa", 3));
     }
 }
