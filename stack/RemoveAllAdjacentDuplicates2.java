55e041eddbb36f6e89d953fa7e4f69c7d69e1f1c
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-03-06 15:28:51 +0700

    Solved RemoveAllAdjacentDuplicates2

--- stack/RemoveAllAdjacentDuplicates2.java
+++ stack/RemoveAllAdjacentDuplicates2.java
@@ -1,5 +1,7 @@
 package stack;
 
+import java.util.Stack;
+
 // You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, causing the left and the right side of the deleted substring to concatenate together.
 // We repeatedly make k duplicate removals on s until we no longer can.
 // Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
@@ -50,11 +52,38 @@ public class RemoveAllAdjacentDuplicates2 {
         return sb.toString();
     }
 
+    // Stack approach: push into a stack a int[] to store char and the consecutive appear number
+    public static String removeDuplicates2(String s, int k) {
+        Stack<int[]> st = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (!st.isEmpty() && st.peek()[0] == c) {
+                st.peek()[1]++;
+            } else {
+                st.push(new int[]{c, 1});
+            }
+            
+            if (st.peek()[1] == k) {
+                st.pop();
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            int[] arr = st.pop();
+            for (int i = 0; i < arr[1]; i++) { 
+                sb.append((char) arr[0]);
+            }
+        }
+
+        return sb.reverse().toString();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Test case expected abcd: " + removeDuplicates1("abcd", 2));
-        System.out.println("Test case expected aa: " + removeDuplicates1("deeedbbcccbdaa", 3));
-        System.out.println("Test case expected ps: " + removeDuplicates1("pbbcggttciiippooaais", 2));
-        System.out.println("Test case expected abd: " + removeDuplicates1("abccccd", 2));
+        System.out.println("Test case expected abcd: " + removeDuplicates2("abcd", 2));
+        System.out.println("Test case expected aa: " + removeDuplicates2("deeedbbcccbdaa", 3));
+        System.out.println("Test case expected ps: " + removeDuplicates2("pbbcggttciiippooaais", 2));
+        System.out.println("Test case expected abd: " + removeDuplicates2("abccccd", 2));
         System.out.println(
                 "Test case expected ybth: " + removeDuplicates1("yfttttfbbbbnnnnffbgffffgbbbbgssssgthyyyy", 4));
     }
