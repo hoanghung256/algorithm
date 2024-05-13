57adb97029f887e9bb94017676d03450fb1efaba
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-05-13 14:19:08 +0700

    Util function to print arr

---
+++ utils/Functions.java
@@ -0,0 +1,20 @@
+package utils;
+
+public class Functions {
+
+    public static String printArr(int[] arr) {
+        StringBuilder result = new StringBuilder();
+
+        result.append("[");
+        for (int i = 0; i < arr.length; i++) {
+            result.append(arr[i]);
+            if (i == arr.length - 1) {
+                result.append("]");
+                break;
+            }
+            result.append(",");
+        }
+
+        return result.toString();
+    }
+}
